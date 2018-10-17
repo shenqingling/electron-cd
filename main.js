d3a0a4e9f7eaf9773a41cd30174e3cd848f1f99e
沈庆玲 <sql370@qq.com>
2018-10-17 16:47:57 +0800

    productName改成知氏老师 & 替换icon

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   // mainWindow.loadFile('index.html')
-  mainWindow.loadURL('https://app.chyeth.com');
+  mainWindow.loadURL('https://www.chyeth.com');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
