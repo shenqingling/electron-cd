525e8d2712f0865a0eae8963e12c21ddc9420d0f
沈庆玲 <sql370@qq.com>
2019-03-12 18:52:15 +0800

    CD项目撤回

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   // mainWindow.loadFile('index.html')
-  mainWindow.loadURL('https://www.chyeth.com');
+  mainWindow.loadURL('https://course.chyeth.com');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
